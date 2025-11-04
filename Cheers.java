1e173bf9589ae0f08e71f30e6eb73a6b39f86e8a
omerheifetz <omer.heifetz@post.runi.ac.il>
2025-11-04 17:42:45 +0200

    HW2-OMERHEIFETZ-REPAIR3

--- Cheers.java
+++ Cheers.java
@@ -4,33 +4,34 @@
 Great work boaz!
 */
 
-
+// change to capital lettars
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    String sIn= args [0];
-            int N= Integer.parseInt(args[1]);
+	    String sIn = args [0];
+            String sIn1 = sIn.toUpperCase();
+            int N = Integer.parseInt(args[1]);
             String checkLetters = "AEFHILMNORSX";
             boolean check = false;
             //give me
             for (int i=0; i<sIn.length(); i++) {
                 for (int j=0; j<checkLetters.length(); j++) {
-                        if (sIn.charAt(i)==checkLetters.charAt(j)) {
+                        if (sIn1.charAt(i)==checkLetters.charAt(j)) {
                         check = true;
                         }
                 }
                 if (check) {
-                        System.out.println("Give me an " + sIn.charAt(i) + ": " + sIn.charAt(i) + "!");
+                        System.out.println("Give me an " + sIn1.charAt(i) + ": " + sIn1.charAt(i) + "!");
                         }
                         else {
-                        System.out.println("Give me a  " + sIn.charAt(i) + ": " + sIn.charAt(i) + "!");
+                        System.out.println("Give me a  " + sIn1.charAt(i) + ": " + sIn1.charAt(i) + "!");
                         }
                 check=false;
             }
 
             System.out.println("What does that spell?");
             for (int i=0; i<N; i++) {
-                System.out.println(sIn + "!!!");
+                System.out.println(sIn1 + "!!!");
             }
         }
 }
